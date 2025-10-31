4708d14d2da07753da4d0d7d2587e5be452f4a72
Joedel <joedel.rf@gmail.com>
2025-10-31 21:49:23 +0800

    add otp api

--- src/api/messageHandler.php
+++ src/api/messageHandler.php
@@ -1,58 +1,83 @@
-<?php 
-
-require __DIR__ . "../../../vendor/autoload.php";
-
-// $client = new GuzzleHttp\Client(); 
-
-$apiKey = "6216B84B7051465CB62FFC9C55767F6E";
-$apiSecret = "C6pUnqgfl3OwYGf9E0zpY03a7MVlH6IreUrSC6X0";
-
-// $response = $client->request("POST", "https://api.cloudsms.io/v1/messages", [
-//     "headers" => [
-//         "Content-type" => "application/json"
-//     ],
-//     "auth" => [$apiKey, $apiSecret],
-//     "json" => [
-//         "destination" => "09949751617",
-//         "message" => "Your One-Time PIN is {otp}. Please do not share this with anyone. Ref# ABC-000000"
-//     ]
-// ]);
-
-// if ($response->getStatusCode() == 200) {
-//     print_r($response->getBody());
-// }
-
-// $client = new GuzzleHttp\Client(); 
-
-// $response = $client->request("POST", "https://api.cloudsms.io/v1/messages", [
-//     "headers" => [
-//         "Content-type" => "application/json"
-//     ],
-//     "auth" => [$apiKey, $apiSecret],
-//     "json" => [
-//         "destination" => "09949751617",
-//         "message" => "Sample text message",
-//         "type" => "sms"
-//     ]
-// ]);
-
-// if ($response->getStatusCode() == 200) {
-//     print_r($response->getBody());
-// }
-
-// ?>
-
 <?php
-$mocean = new \Mocean\Client(
-        new \Mocean\Client\Credentials\Basic(['apiToken' => 'API_TOKEN_HERE'])
-);
 
-$result = $mocean->message()->send([
-    'mocean-to' => '60123456789',
-    'mocean-from' => 'MOCEAN',
-    'mocean-text' => 'Hello World',
-    'mocean-resp-format' => 'json'
-]);
+class smsHandler {
+    private $smsUrl = 'https://sms.iprogtech.com/api/v1/sms_messages';
+    private $otpUrl = 'https://sms.iprogtech.com/api/v1/otp/send_otp';
+    private $apiToken = '92b811ed5d06c8640a7122f69ac1eac583b41b55';
 
-echo $result;
+    function sendSms($dataArray) {
+
+        $data = [
+        'api_token' => $this->apiToken,
+        'message' => $dataArray['message'],
+        'phone_number' => $dataArray['phoneNunber']
+        ];
+        
+        $ch = curl_init($this->smsUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/x-www-form-urlencoded'
+        ]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+
+    function sendOtp($number) {
+        $data = [
+        'api_token' => $this->apiToken,
+        'phone_number' => $number
+        ];
+        
+        $ch = curl_init($this->otpUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/x-www-form-urlencoded'
+        ]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+
+    function verfyOtp($num, $otp) {
+        $url = 'https://sms.iprogtech.com/api/v1/otp/verify_otp';
+
+        $data = [
+            "api_token" => $this->apiToken,
+            "phone_number" => $num,
+            "otp" => $otp
+        ];
+        
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/x-www-form-urlencoded'
+        ]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+
+
+}
+
+
+$otps = new smsHandler();
+// echo $otps->sendOtp('09949751617');
+
+echo $otps->verfyOtp('09949751617', '835624');
+echo "<br>";
+echo $otps->verfyOtp('09949751617', '835623');
+
+ 
+?>
+
+
+
 ?>
---
+++ src/api/otp.php
@@ -0,0 +1,6 @@
+<!-- uhhh this one generate otp  -->
+<?php
+
+require_once __DIR__ .'/messageHandler.php';
+
+?>
